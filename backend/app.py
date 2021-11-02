4820d1bd011def04ce77f596bd25e37ac0130bbe
suyeon <pisuyeun@gmail.com>
2021-11-02 11:24:42 +0900

    pymongo setting

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,23 @@
-from flask import Flask
+from pymongo import MongoClient
+
+from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.airtravel
+doc = {
+	"file": 'url',
+	"location": 'Seoul',
+	"continent": 'Asia',
+	"date": '21-11-02',
+	"content": 'fun and interesting'
+}
+db.card.insert_one(doc)
 
 @app.route('/')
 def home():
+
 	return 'This is Home!'
 
 if __name__ == '__main__':
