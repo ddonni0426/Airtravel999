dde9fb6696b6625a130f77d970f650f23113f53c
suyeon <pisuyeun@gmail.com>
2021-11-02 23:20:11 +0900

    make tour api and add gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,14 @@
-from pymongo import MongoClient
-
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 
 from like import like
 from auth import auth
 from tour import tour
-
 
 app = Flask(__name__, template_folder='../frontend/src')
 app.register_blueprint(tour, url_prefix="/tour")
 app.register_blueprint(like, url_prefix="/like")
 app.register_blueprint(auth, url_prefix="/auth")
 
-
-client = MongoClient('localhost', 27017)
-db = client.airtravel
-doc = {
-	"file": 'url',
-	"location": 'Seoul',
-	"continent": 'Asia',
-	"date": '21-11-02',
-	"content": 'fun and interesting'
-}
-db.card.insert_one(doc)
 
 @app.route('/')
 def home():
