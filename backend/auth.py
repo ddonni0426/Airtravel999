069308bff677466f7ef4fb296b32048172e08114
silica67sdm <1127lse@gmail.com>
2021-11-03 09:48:15 +0900

    [auth] create auth cheking user info api

--- backend/auth.py
+++ backend/auth.py
@@ -42,10 +42,26 @@ def api_login():
     if result is not None:
         payload = {
             'id': id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=3)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60*60*60)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
     else:
         return jsonify({'result': 'fail', 'msg': '회원정보 오류입니다.'})
+
+# 유저정보 확인 API
+@auth.route('/user', methods=['GET'])
+def api_valid():
+    token_receive = request.cookies.get('mytoken')
+
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        print(payload)
+
+        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
