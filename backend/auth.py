ed939f488aca20ff9d37a8f53865a1fd55deebb8
silica67sdm <1127lse@gmail.com>
2021-11-03 09:33:04 +0900

    [auth] create auth login api using jwt

--- backend/auth.py
+++ backend/auth.py
@@ -6,17 +6,17 @@ auth = Blueprint("auth", __name__)
 client = MongoClient('localhost', 27017)
 db = client.airtravel
 
-# SECRET_KEY = "AirTravel_AweSome_Team"
+SECRET_KEY = "AirTravel_AweSome_Team"
 
-# import jwt
-# import datetime
+import jwt
+import datetime
 import hashlib
 
 @auth.route('/')
 def getTour():
   return "auth"
 
-
+# 회원가입 API
 @auth.route("/register", methods=["POST"])
 def api_register():
     id = request.form["id"]
@@ -28,3 +28,24 @@ def api_register():
     db.user.insert_one({"id": id, "pw": pw_hash, "nick": nickname})
 
     return jsonify({"result": "success"})
+	
+# 로그인 API
+@auth.route('/login', methods=['POST'])
+def api_login():
+    id = request.form['id']
+    pw = request.form['pw']
+
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+
+    result = db.user.find_one({'id': id, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=3)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    else:
+        return jsonify({'result': 'fail', 'msg': '회원정보 오류입니다.'})
